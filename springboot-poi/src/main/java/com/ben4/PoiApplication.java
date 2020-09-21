558ef3eec7abdd5815795a7e48b039bafde45988
kgcaox <740012297@qq.com>
2020-09-21 17:15:25 +0800

    redis的分布式锁，队列，lua使用示例

---
+++ springboot-poi/src/main/java/com/ben4/PoiApplication.java
@@ -0,0 +1,18 @@
+package com.ben4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: caokang
+ * @Date: Created in 16:42 2020/6/30
+ * @annotation:接口
+ * @version:1.0
+ * @TableName:
+ */
+@SpringBootApplication
+public class PoiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PoiApplication.class, args);
+    }
+}
